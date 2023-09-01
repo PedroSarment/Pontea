8887f81989bb5f71bed70c6702112ea1c5ab8a21
PedroSarment <phsp@cin.ufpe.br>
2023-09-01 11:40:00 -0300

    Cria Areas Model

---
+++ pontea_api/database/seeders/AreasSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AreasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        $areas = [
+            'O eu, o outro e o nós',
+            'Corpo, gestos e movimentos',
+            'Traços, sons, cores e formas',
+            'Escuta, fala, pensamento e imaginação',
+            'Espaços, tempos, quantidades, relações e transformações',
+        ];
+
+        foreach ($areas as $areaTitle) {
+            // Verifique se a área já existe na tabela
+            $existingArea = Area::where('title', $areaTitle)->first();
+
+            // Se a área não existir, crie-a
+            if (!$existingArea) {
+                Area::create([
+                    'title' => $areaTitle,
+                ]);
+            }
+        }
+    }
+}
